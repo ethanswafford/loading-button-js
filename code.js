8b30c3ed702c77faa70eb5967a02432abca4452e
Ethan Swafford <ethanswafford@yahoo.com>
2022-10-26 01:21:10 -0500

    devPush

--- code.js
+++
@@ -1,9 +0,0 @@
-console.log("connected")
-const searchButton = document.querySelector('#search-button')
-const searchButtonContent = document.querySelector('#search-button div')
-
-searchButton.addEventListener('click', toggleButton)
-
-function toggleButton() {
-    searchButtonContent.toggle('loading')
-}
